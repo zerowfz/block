94751b6f17009c822409bca89442c7458a307195
wfz <649181308@qq.com>
2018-09-10 14:52:06 +0800

    update

--- non_local_layer.cpp
+++ non_local_layer.cpp
@@ -6,19 +6,30 @@ namespace caffe{
 template <typename Dtype>
 void LocalLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
 		const vector<Blob<Dtype>*>& top){
-  CHECK_EQ(bottom.size(),2)<<"nonlocal layer takes two blobs as input";
-  CHECK_EQ(top.size(),2)<<"nonlocal layer takes two blobs as output";
+  CHECK_EQ(bottom.size(),2)<<"number of input should be 2";
+  CHECK_EQ(top.size(),2)<<"number of output should be 2";
+  CHECK_EQ(bottom[0]->num(),bottom[1]->num())<<"input should has same batchsize";
+  CHECK_EQ(bottom[0]->height(),bottom[1]->height())<<"input should has same height";
+  CHECK_EQ(bottom[0]->width(),bottom[1]->width())<<"input should has same width";
 
-  neibor_size_ = this->layer_param_.nonlocal_param().neibor_size();
-  conv_out1_ = this->layer_param_.nonlocal_param().conv_out1();
-  conv_out2_ = this->layer_param_.nonlocal_param().conv_out2();
+  num_ = bottom[0]->num();
+  fea_channel_ = bottom[0]->channel();
+  corr_channel_ = bottom[1]->channel();// neibor size q*q
+  height_ = bottom[0]->height();
+  width_ = bottom[0]->width();
   
-  conv1_layer_.clear();
-  conv2_layer_.clear();
-  conv3_layer_.clear();
+  infea_dim_ = this->layer_param_.nonlocal_param().conv_output();
 
-  CHECK_EQ(bottom[0]->num(),bottom[1]->num())<<"input has diffenrent batchsize";
-  CHECK_EQ(bottom[0]->)
+
+  LayerParameter conv_param1 = 
+  LayerParameter conv_param2 =
+  LayerParameter conv_param3 = 
+  conv1_layers_.reset()
+  conv1_layers->SetUp(bottom[0],conv1_top_);
+  covn2_layers.reset()
+  conv2_layers->SetUp(bottom[0],conv2_top_);
+  conv3_layers.reset()
+  conv3_layers->SetUp(bottom[0],conv3_top_);
 }
 
 }
